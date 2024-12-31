baa16926f75642d88552c3e2661f845bc71027d5
Nishchal Ravi <nxckmusicprod@gmail.com>
2024-12-31 16:54:19 +0530

    Clear screen on exit points

--- lol.c
+++ lol.c
@@ -24,6 +24,9 @@ struct termios original_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -107,6 +110,8 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
